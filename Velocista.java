f444df9c43185cdd3f132b13e002e8d623e2a397
Jhony Alpala <jhoniher95@gmail.com>
2023-10-16 18:24:50 -0500

    Agragada clase Velocista

--- Velocista.java
+++ Velocista.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ej_4_8_carreraciclistica;
 
-/**
- *
- * @author Jhony
- */
-public class Velocista {
+public class Velocista extends Ciclista {
+    private double potenciaPromedio;
+    private double velocidadPromedio;
     
+    public Velocista(int identificador, String nombre, double potenciaPromedio, double velocidadPromedio){
+        super(identificador, nombre);
+        this.potenciaPromedio=potenciaPromedio;
+        this.velocidadPromedio=velocidadPromedio;
+    }
+    protected double getPotenciaPromedio(){
+        return potenciaPromedio;
+    }
+    protected void setPotenciaPromedio(double potenciaPromedio){
+        this.potenciaPromedio=potenciaPromedio;
+    }
+    protected double getvelocidadPromedio(){
+        return velocidadPromedio;
+    }
+    protected void setVelocidadPromedio(double velocidadPromedio){
+        this.velocidadPromedio=velocidadPromedio;
+    }
+    protected void imprimir(){
+        super.imprimir();
+        System.out.println("Potencia promedio="+potenciaPromedio);
+        System.out.println("Velocidad promedio="+velocidadPromedio);
+    }
+    protected String imprimirTipo(){
+        return "Es un velocista";
+    }
 }
