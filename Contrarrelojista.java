40dc7e24de2a34645cbea7f9deefeb632b46431f
Jhony Alpala <jhoniher95@gmail.com>
2023-10-16 19:41:12 -0500

    Finalizada clase Contrarrelojista

--- Contrarrelojista.java
+++ Contrarrelojista.java
@@ -1,13 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ej_4_8_carreraciclistica;
 
-/**
- *
- * @author Jhony
- */
-public class Contrarrelojista {
-    
+public class Contrarrelojista extends Ciclista{
+    private double velocidadMaxima;
+    public Contrarrelojista(int identificador, String nombre, double velocidadMaxima){
+        super(identificador, nombre);
+        this.velocidadMaxima=velocidadMaxima;
+    }
+    protected double getVelocidadMaxima(){
+        return velocidadMaxima;
+    }
+    protected void setVelocidadMaxima(double velodidadMaxima){
+        this.velocidadMaxima=velocidadMaxima;
+    }
+    protected void imprimir(){
+        super.imprimir();
+        System.out.println("Aceleracion promedio="+velocidadMaxima);
+    }
+    protected String imprimirTipo(){
+        return "Es un contrarrelojista";
+    }
 }
